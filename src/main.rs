8804f781bba7b5904927f4a5cf053117ffe34965
Rustam <irzaevrustam@gmail.com>
2024-08-08 20:14:54 +0200

    added simple spinner

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use image::{png::PNGEncoder, ColorType};
 use models::PlotColorSchema;
 use parse::{parse_complex, parse_pair};
 use std::{fs::File, str::FromStr};
+use std::time::Duration;
+use indicatif::{ProgressBar, ProgressStyle};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -30,9 +32,30 @@ fn main() {
     let color_schema = PlotColorSchema::from_str(&args[5]).expect("error parsing color schema");
     let rows_per_band = bounds.1 / num_cpus::get() + 1;
 
-    let pixels = generate(rows_per_band, bounds, upper_left, lower_right, color_schema);
+    let pb = create_progress_bar();
 
+    let pixels = generate(rows_per_band, bounds, upper_left, lower_right, color_schema);
     write_image(&args[1], &pixels, bounds, color_schema).expect("error writing PNG file");
+
+    pb.set_message("Done!");
+}
+
+fn create_progress_bar() -> ProgressBar {
+    let pb = ProgressBar::new_spinner();
+    pb.enable_steady_tick(Duration::from_millis(120));
+    pb.set_message("Generating image ...");
+    pb.set_style(
+        ProgressStyle::with_template("{spinner:.blue} {msg}")
+            .unwrap()
+            .tick_strings(&[
+                "▸▹▹",
+                "▹▸▹",
+                "▹▹▸",
+                "▪▪▪",
+            ]),
+    );
+
+    pb
 }
 
 fn write_image(
